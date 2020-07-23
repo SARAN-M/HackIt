32230456b11a1963738d8b0096b17f5d60cb8b27
SARAN M <31407429+SARAN-M@users.noreply.github.com>
2020-07-23 09:31:15 +0530

    Update faculty.js

--- routes/faculty.js
+++ routes/faculty.js
@@ -4,15 +4,16 @@ require('../models/Form');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const connectEnsureLogin = require('connect-ensure-login');
 const mongoose = require('mongoose');
 const Handlebars = require('handlebars')
 const exphbs = require('express-handlebars');
+const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 
 const test = mongoose.model('Testdetail');
 
 var app = express();
+const User = mongoose.model('Users');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
@@ -20,24 +21,27 @@ app.use(bodyParser.urlencoded({
 app.engine('hbs', exphbs({ extname: 'hbs', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
 app.set('view engine', 'hbs');
 
-app.get('/', connectEnsureLogin.ensureLoggedIn(),(req, res) => {
-    res.render('dashboard.hbs', {layout: false});
-});
+
+app.get('/', ensureAuthenticated,(req, res) => {
+    User.find({email:req.user.email},(err,docs)=>{
+        if(docs[0].stat=='Staff'){
+        res.render('dashboard.hbs', {layout: false});}
+        else if(docs[0].stat=='Student'){
+        res.redirect('/student');
+        }
+    })
+  });
 
 var form = require('./createTest.js');
 app.use('/createTest', form);
 
 var uploads = require('../middleware/uploadController');
-app.get('/viewTest', uploads.testAvail);
-
-app.get('/:title', uploads.loadHome);
+app.get('/uploadTest',uploads.loadHome);
 
 app.post('/uploadTest/upload', uploads.uploadFile);
 
 app.post('/created', (req, res) => {
-    res.redirect('/faculty');;
+    res.redirect('/faculty');
 });
-
-app.get('/:title/view', uploads.viewTest);
 
 module.exports = app;
