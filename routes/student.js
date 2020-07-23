79ddb8733a7cbc50e9051c19ef49519fd98c9350
SARAN M <31407429+SARAN-M@users.noreply.github.com>
2020-07-23 09:36:56 +0530

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -4,14 +4,28 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const Handlebars = require('handlebars');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
-
+const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
+const User = mongoose.model('Users');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.get('/student',ensureAuthenticated,(req, res) => {
+    User.find({email:req.user.email},(err,docs)=>{
+        console.log(req.user.email);
+        if(docs[0].stat=='Student'){
+            res.render('student.hbs', {layout: false});
+        }
+        else{
+            res.redirect('/');
+        }
+    })
+   
+  });
 app.engine('hbs', exphbs({ extname: 'hbs', handlebars: allowInsecurePrototypeAccess(Handlebars), helpers:{
     // Function to do basic mathematical operation in handlebar
     math: function(lvalue, operator, rvalue) {lvalue = parseFloat(lvalue);
@@ -29,9 +43,9 @@ app.set('view engine', 'hbs');
 
 var test = require('../middleware/testController');
 
-app.get('/:title', test.paginatedResults);
+app.get('/:title',ensureAuthenticated, test.paginatedResults);
 
-app.use('/', test.testAvail);
+app.use('/',ensureAuthenticated, test.testAvail);
 
 
 module.exports = app;
